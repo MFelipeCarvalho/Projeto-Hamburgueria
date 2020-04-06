bc7f4a79f9f4697eb8c5f0e7ab7232293f407e5b
Marcos Felipe Barbosa de Carvalho <63249409+MFelipeCarvalho@users.noreply.github.com>
2020-04-06 18:43:50 -0300

    Create indexPromocoes.php

---
+++ pages/indexPromocoes.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Honker Burger</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
+        <link rel="stylesheet" type="text/css" href="../css/reset.css">
+        <link rel="icon" href="../imagens/favicon.ico"/>
+        <script src="../js/jquery.js"></script>
+        <script src="../js/ancora.js"></script>
+    </head> 
+    
+    <body>
+    
+        
+        <!-- Área do cabeçalho -->
+        
+        <div class="divCabecalho">
+            <div class="divConteudoCabecalho">
+                <div class="divLogo">
+                    <figure>
+                        <a href="../index.php">
+                        <img class="logoImagem" src="../imagens/logo.png" alt="Logo imagem">
+                        </a>
+                    </figure>   
+                </div>
+                
+                <!-- Área do menu -->
+                
+                <div class="divBoxBotoesMenu" >
+                    
+                    <div class="divBotaoMenu">
+                        <p class="pMenuItemCabecalho"><a href="indexProdutosDoMes.php">Mês</a></p>
+                    </div>
+                    
+                    <div class="divBotaoMenu">
+                        <p class="pMenuItemCabecalho"><a href="indexCuriosidades.php">Sabia?</a></p>
+                    </div>
+                    
+                    <div class="divBotaoMenu">
+                        <p class="pMenuItemCabecalho"><a href="indexLojas.php">Lojas</a></p>
+                    </div>
+                    
+                    <div class="divBotaoMenu">
+                        <p class="pMenuItemCabecalho"><a href="indexPromocoes.php">Desconto</a></p>
+                    </div>
+                    
+                    <div class="divBotaoMenu">
+                        <p class="pMenuItemCabecalho"><a href="indexContato.php">Contato</a></p>
+                    </div>
+                    
+                    <div class="divBotaoMenu">
+                        <p class="pMenuItemCabecalho"><a href="indexInfo.php">Sobre</a></p>
+                    </div>
+                        
+                    </div>
+                
+                <!-- Área do login -->
+                
+                 <div class="divBoxLogin">
+                
+                    <form>
+                        
+                        <input class="formLoginESenha" type="text" name="usuario" value="Usuário">
+                        
+                        <input class="formLoginESenha" type="text" name="senha" value="Senha">
+                    
+                        <input class="botaoLogin" type="button" name="botaoLogin" value="Login">
+                        
+                    </form>
+                    
+                </div>
+     
+            </div>
+        </div>
+        
+        <!-- Área das promoções -->
+        
+        <div class="divBoxFullPromocoes">
+            <div class="divBoxPromocoes">
+                <div class="divPromocao">
+                    <div class="divBoxImagemPromocao">
+                        <img class="imagemPromocao" src="../imagens/imagemModeloPromocoes.jpg" alt="Item promoção">
+                    </div>
+                </div>
+                
+                <div class="divPromocao">
+                    <div class="divBoxImagemPromocao">
+                          <img class="imagemPromocao" src="../imagens/imagemModeloPromocoes.jpg" alt="Item promoção">
+                    </div>
+                </div>
+                
+                <div class="divPromocao">
+                    <div class="divBoxImagemPromocao">
+                          <img class="imagemPromocao" src="../imagens/imagemModeloPromocoes.jpg" alt="Item promoção">
+                    </div>
+                </div>
+                
+                <div class="divPromocao">
+                    <div class="divBoxImagemPromocao">
+                          <img class="imagemPromocao" src="../imagens/imagemModeloPromocoes.jpg" alt="Item promoção">
+                    </div>
+                </div>
+                
+                <div class="divPromocao">
+                    <div class="divBoxImagemPromocao">  
+                          <img class="imagemPromocao" src="../imagens/imagemModeloPromocoes.jpg" alt="Item promoção">
+                    </div>
+                </div>
+                
+                <div class="divPromocao">
+                    <div class="divBoxImagemPromocao">
+                        <img class="imagemPromocao" src="../imagens/imagemModeloPromocoes.jpg" alt="Item promoção">
+                    </div>
+                </div>
+            </div>
+        </div>
+                
+         <!-- Área do rodapé -->
+        
+        <div class="divBoxFullRodape">
+            <div class="divBoxRodape">  
+                <div class="divBoxSistemaInterno">
+                    <form>
+                        <input class="botaoSistemaInternoCSS" type="button" name="botaoSistemaDownload" value="Sistema Interno">
+                    </form>
+                </div>
+                <div class="divBoxEndereco">
+                    <p class="endereco">Av. Luis Carlos Berrini, nº 666. Itaim Bibi, São Paulo-SP</p>
+                </div>
+                <div class="divBoxDownload">
+                    <div class="boxIconePlayStore">
+                        <img class="imgLogoPlaystore" src="../imagens/logoPlayStore.png" alt="Play Store logo">
+                    </div>
+                    <div class="divBoxBotaoDowload"> 
+                        <form>
+                            <input class="botaoDownload" type="button" name="botaoDownload" value="Baixe o App">
+                        </form>                        
+                    </div>
+                </div>  
+            </div>  
+        </div>
+        
+    </body>
+</html>
