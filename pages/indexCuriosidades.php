aa92a745597821a48561523d173ed31ba7447026
Marcos Felipe Barbosa de Carvalho <63249409+MFelipeCarvalho@users.noreply.github.com>
2020-04-06 18:41:55 -0300

    Create indexCuriosidades.php

---
+++ pages/indexCuriosidades.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Honker Burger</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
+        <link rel="stylesheet" type="text/css" href="../css/reset.css">
+        <link rel="icon" href="../imagens/favicon.ico"/>
+        <script src="../js/jquery.js"></script>
+        <script src="../js/ancora.js"></script>
+    </head> 
+    
+    <body>
+    
+        
+        <!-- Área do cabeçalho -->
+        
+        <div class="divCabecalho">
+            <div class="divConteudoCabecalho">
+                <div class="divLogo">
+                    <figure>
+                        <a href="../index.php">
+                        <img class="logoImagem" src="../imagens/logo.png" alt="Logo imagem">
+                        </a>
+                    </figure>   
+                </div>
+                
+                <!-- Área do menu -->
+                
+                <div class="divBoxBotoesMenu" >
+                    
+                    <div class="divBotaoMenu">
+                        <p class="pMenuItemCabecalho"><a href="indexProdutosDoMes.php">Mês</a></p>
+                    </div>
+                    
+                    <div class="divBotaoMenu">
+                        <p class="pMenuItemCabecalho"><a href="indexCuriosidades.php">Sabia?</a></p>
+                    </div>
+                    
+                    <div class="divBotaoMenu">
+                        <p class="pMenuItemCabecalho"><a href="indexLojas.php">Lojas</a></p>
+                    </div>
+                    
+                    <div class="divBotaoMenu">
+                        <p class="pMenuItemCabecalho"><a href="indexPromocoes.php">Desconto</a></p>
+                    </div>
+                    
+                    <div class="divBotaoMenu">
+                        <p class="pMenuItemCabecalho"><a href="indexContato.php">Contato</a></p>
+                    </div>
+                    
+                    <div class="divBotaoMenu">
+                        <p class="pMenuItemCabecalho"><a href="indexInfo.php">Sobre</a></p>
+                    </div>
+                        
+                    </div>
+                
+                <!-- Área do login -->
+                
+                <div class="divBoxLogin">
+                
+                    <form>
+                        
+                        <input class="formLoginESenha" type="text" name="usuario" value="Usuário">
+                        
+                        <input class="formLoginESenha" type="text" name="senha" value="Senha">
+                    
+                        <input class="botaoLogin" type="button" name="botaoLogin" value="Login">
+                        
+                    </form>
+                    
+                </div>
+     
+            </div>
+        </div>
+        
+        <!-- Área Curiosidade -->
+        
+        <div class="divBoxFullCuriosidades">
+            <div class="divBoxCuriosidades">
+                <div class="divBoxCuriosidadesConteudo">
+                    <p class="pInfoCuriosidades">
+                        Você sabia que a origem do hambúrguer pode ser traçada a partir do século XIII? Historiadores relatam que guerreiros da Mongólia amaciavam pedaços de carne sob suas celas e os comiam durante suas jornadas sem ao menos descer de seus cavalos.
+                    </p>
+                </div>
+                <div class="divBoxCuriosidadesConteudo">
+                    <p class="pInfoCuriosidades">
+                        Com a chegada dos mongóis a Moscou, o hábito de consumir carne moída começou a se espalhar pela Europa e evoluiu até tornar-se o prato que conhecemos hoje em dia como steak tartare e que, por sua vez, evoluiu até o Hamburg steak - um disco de carne moída temperada frito.
+                </div>
+                <div class="divBoxCuriosidadesConteudo">
+                     <p class="pInfoCuriosidades">
+                        A relação de amor entre os brasileiros e o hambúrguer começou nos anos 50, quando o tenista americano Robert Falkenburg abriu o primeiro Bob’s no Rio de Janeiro. Podemos dizer que foi amor à primeira vista - o Brasil é um dos países que mais consomem hambúrgueres no mundo!
+                     </p>
+                </div>
+                <div class="divBoxCuriosidadesConteudo">
+                     <p class="pInfoCuriosidades">
+                        Apesar do alto teor calórico com um teor maior de gordura, você pode consumir o lanche. Na hora de comprá-lo podemos pedir salada como acompanhamento. Se estamos cuidando do peso, podemos pedir sem muito recheio, por exemplo, sem maionese ou com menos queijo.  
+                     </p>
+                </div>
+                <div class="divBoxCuriosidadesConteudo">
+                    <p class="pInfoCuriosidades">
+                        O hambúrguer mais caro do mundo custa em torno de $5.000 dólares, aproximadamente R$14.500. O sanduíche é vendido pelo JuicysFoodsandOvations for Juicys Outlaw Grill, nos Estados Unidos. Com esse preço, aqui no Brasil podemos comprar até um carro usado!
+                    </p>
+                </div>
+                <div class="divBoxCuriosidadesConteudo">
+                    <p class="pInfoCuriosidades">
+                        É impossível saber quantos hambúrgueres são consumidos todos os dias no planeta, mas a maior rede de fast foood do mundo calculou os que saem de suas cozinhas. O resultado é assombroso: 12.528.000 hambúrgueres por dia ou 145 por segundo.
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+            <!-- Área do rodapé -->
+        
+        <div class="divBoxFullRodape">
+            <div class="divBoxRodape">  
+                <div class="divBoxSistemaInterno">
+                    <form>
+                        <input class="botaoSistemaInternoCSS" type="button" name="botaoSistemaDownload" value="Sistema Interno">
+                    </form>
+                </div>
+                <div class="divBoxEndereco">
+                    <p class="endereco">Av. Luis Carlos Berrini, nº 666. Itaim Bibi, São Paulo-SP</p>
+                </div>
+                <div class="divBoxDownload">
+                    <div class="boxIconePlayStore">
+                        <img class="imgLogoPlaystore" src="../imagens/logoPlayStore.png" alt="Play Store logo">
+                    </div>
+                    <div class="divBoxBotaoDowload"> 
+                        <form>
+                            <input class="botaoDownload" type="button" name="botaoDownload" value="Baixe o App">
+                        </form>                        
+                    </div>
+                </div>  
+            </div>  
+        </div>
+        
+    </body>
+</html>
